d0655889f1ca3ef75f0a12594e9748a53360069f
DiegoBohorquez05 <judiboor@gmail.com>
2023-02-11 22:26:19 -0500

    js terminado pero solo muestre el ultimo ingresado

--- ejercicioLS/main.js
+++ ejercicioLS/main.js
@@ -1,43 +1,58 @@
-const nombre=document.getElementById('nombre')
-const curso=document.getElementById('Curso')
-const genero=document.getElementById('Genero')
-const agregar=document.getElementById('agregar')
-const mostrar=document.getElementById('mostrar')
-const tabla=document.getElementById('tabla')
+const nombre = document.getElementById("nombre");
+const curso = document.getElementById("Curso");
+const genero = document.getElementById("Genero");
+const agregar = document.getElementById("agregar");
+const mostrar = document.getElementById("mostrar");
+const tabla = document.getElementById("tabla");
 
-const nota1=parseFloat(document.getElementById('Nota1').value)
-const nota2=parseFloat(document.getElementById('Nota2').value)
-const nota3=parseFloat(document.getElementById('Nota3').value)
+let CalificacionFinale = null;
 
-let CalificacionFinale=null
+datos = [];
 
-datos=[]
+agregar.addEventListener("click", () => {
+  const nota1 = parseFloat(document.getElementById("Nota1").value);
+  const nota2 = parseFloat(document.getElementById("Nota2").value);
+  const nota3 = parseFloat(document.getElementById("Nota3").value);
+  if (nota1 > 5 || nota2 > 5 || nota3 > 5) {
+    alert("Las notas no pueden ser mayores a 5");
+  } else {
+    console.log(nota1);
+    console.log(datos);
+    //console.log(promedio)
+    datos.push({
+      nombre: nombre.value,
+      curso: curso.value,
+      genero: genero.value,
+      Nota1: nota1,
+      Nota2: nota2,
+      Nota3: nota3,
+    });
+    localStorage.setItem("estudiantes", JSON.stringify(datos));
+  }
 
-agregar.addEventListener('click',()=>{
-        if(nota1>5 || nota2>5 || nota3>5){
-            alert('Las notas no pueden ser mayores a 5')
-        }else{
-            console.log(datos)
-            //console.log(promedio)
-            datos.push({nombre: nombre.value, curso: curso.value, genero: genero.value, Nota1: nota1, Nota2: nota2, Nota3: nota3})
-            localStorage.setItem('estudiantes',JSON.stringify(datos))
-        }
-})
-
-mostrar.addEventListener('click',()=>{
-    lista=JSON.parse(localStorage.getItem('estudiantes'))
-    console.log(lista)
-    if(lista==undefined){
-        alert('Lista Vacia')
-    }else{
-        promedio=(nota1+nota2+nota3)/3
-        if(promedio>=3.5){
-            CalificacionFinale='Aprovado'
-        }else{
-            CalificacionFinale='Reprovado'
-        }
-        lista.forEach(element => {
-        tabla.innerHTML= element.nombre + " , " + "Promedio: " + promedio + " , " + CalificacionFinale
-       });
+  mostrar.addEventListener("click", () => {
+    lista = JSON.parse(localStorage.getItem("estudiantes"));
+    console.log(lista);
+    if (lista == undefined) {
+      alert("Lista Vacia");
+    } else {
+      promedio = (nota1 + nota2 + nota3) / 3;
+      if (promedio >= 3.5) {
+        CalificacionFinale = "Aprovado";
+      } else {
+        CalificacionFinale = "Reprovado";
+      }
+      lista.forEach((element) => {
+        tabla.innerHTML =
+          element.nombre +
+          " , " +
+          "Promedio: " +
+          promedio +
+          " , " +
+          CalificacionFinale;
+      });
     }
-})
+  });
+});
+
+
